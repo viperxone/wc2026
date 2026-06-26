e0f9dd19493042d17fd2e9c3fcb93db807696154
Ahmad Ali <ahmadaliviper@gmail.com>
2026-06-26 14:28:56 +0800

    Update provider.js

--- js/provider.js
+++ js/provider.js
@@ -1,1 +1,16 @@
+const ESPN_SCOREBOARD =
+  "https://site.api.espn.com/apis/site/v2/sports/soccer/fifa.world/scoreboard";
 
+class ESPNProvider {
+  static async getMatches() {
+    try {
+      const res = await fetch(ESPN_SCOREBOARD);
+      const data = await res.json();
+
+      return data.events || [];
+    } catch (err) {
+      console.error("ESPN fetch failed", err);
+      return [];
+    }
+  }
+}
