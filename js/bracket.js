20ee02026c3b3046bd3b5967781378ceca699977
Ahmad Ali <ahmadaliviper@gmail.com>
2026-06-26 17:45:21 +0800

    Update bracket.js

--- js/bracket.js
+++ js/bracket.js
@@ -1,1 +1,28 @@
+class BracketEngine {
 
+  static generate(data) {
+
+    const teams =
+      data.qualified
+      .map(x=>x.team);
+
+    while(teams.length < 32){
+      teams.push("TBD");
+    }
+
+    const matches=[];
+
+    for(let i=0;i<32;i+=2){
+
+      matches.push({
+        home:teams[i],
+        away:teams[i+1]
+      });
+
+    }
+
+    return matches;
+
+  }
+
+}
