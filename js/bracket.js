df0303f17ba2d68a3d6afb55a04377dda30ebdf3
Ahmad Ali <ahmadaliviper@gmail.com>
2026-06-26 18:09:00 +0800

    Update bracket.js

--- js/bracket.js
+++ js/bracket.js
@@ -2,27 +2,36 @@ class BracketEngine {
 
   static generate(data) {
 
-    const teams =
-      data.qualified
-      .map(x=>x.team);
+    const teams = [];
 
-    while(teams.length < 32){
+    if (data?.qualified) {
+      data.qualified.forEach(q => {
+        teams.push(q.team);
+      });
+    }
+
+    if (data?.bestEight) {
+      data.bestEight.forEach(q => {
+        teams.push(q.team);
+      });
+    }
+
+    while (teams.length < 32) {
       teams.push("TBD");
     }
 
-    const matches=[];
+    const matches = [];
 
-    for(let i=0;i<32;i+=2){
+    for (let i = 0; i < 32; i += 2) {
 
       matches.push({
-        home:teams[i],
-        away:teams[i+1]
+        home: teams[i],
+        away: teams[i + 1]
       });
 
     }
 
     return matches;
-
   }
 
 }
