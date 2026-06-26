b4bb3fee2248f7d827d04c1b155ce65d8d4f2a2f
Ahmad Ali <ahmadaliviper@gmail.com>
2026-06-26 17:46:35 +0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,14 +1,32 @@
 async function init() {
+
   const matches =
     await ESPNProvider.getMatches();
 
   renderMatches(matches);
+
+  const standings =
+    StandingsEngine.build(matches);
+
+  renderGroups(standings);
+
+  const qualified =
+    QualificationEngine
+    .getQualified(standings);
+
+  const bracket =
+    BracketEngine
+    .generate(qualified);
+
+  renderBracket(bracket);
+
 }
 
 init();
 
 if ("serviceWorker" in navigator) {
+
   navigator.serviceWorker
-    .register("./sw.js")
-    .catch(console.error);
+    .register("./sw.js");
+
 }
